fbbc6bb00d66b0d12953628892bd19f3af37823b
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-03-08 14:55:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Translate } from './components/Translate';
+
+function App() {
+  return (
+    <div>
+      <Translate/>
+    </div>
+  );
+}
+
+export default App;
